f43c7770a1392c8954891a3816b6fb1899cb6fe8
Ogie Nurdiana <ogi@recis.co.id>
2022-10-11 09:34:34 +0700

    fix update level

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 date_default_timezone_set('Asia/Jakarta');
 
 ini_set('set_time_limit', 300);
 ini_set('memory_limit', '1024M');
 ini_set('max_execution_time', 43200);
 
-define('DEBUG', 1);
+define('DEBUG', 0);
 
 require dirname(__FILE__) . '/vendor/autoload.php';
 require('inc/config.php');
@@ -22,11 +22,26 @@ $reader->setActiveSheetIndex(0);
 
 $number = 1;
 $number_sleep = 1;
+
+function level($number) {
+    if ($number <= 20) {
+        return "Very Low";
+    } elseif ($number > 20 && $number <= 40) {
+        return "Low";
+    } elseif ($number > 40 && $number <= 60) {
+        return "Medium";
+    } elseif ($number > 60 && $number <= 80) {
+        return "High";
+    } else {
+        return "Very High";
+    }
+}
 while($number > 0) {
     $cells_no = $reader->getActiveSheet()->getCell('A'.($number+1))->getValue();
     $cells_tag = $reader->getActiveSheet()->getCell('B'.($number+1))->getValue();
     $cells_ems = $reader->getActiveSheet()->getCell('C'.($number+1))->getValue();
 
+    if ($cells_ems == "0") $cells_ems = true;
     if(empty($cells_tag) && empty($cells_ems)) {
         echo "file ".FILE_EXCEL." not data or is blank\n"; break;
     }
@@ -47,13 +62,35 @@ while($number > 0) {
         $cells = 'C';
         $cells_tag = trim($cells_tag);
         foreach ((array) $vidiq->Json('decode', $msg->response)->search_stats->compvol->$cells_tag as $key => $value) {
-            if ($key != 'volume') {
-                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $value);
+            if ($key == 'volume') {
+                $volume = level(ceil($value));
+            }
+
+            if ($key == "overall") {
+                $value = number_format($value, 0, '.', ',');
+                $overall = level(ceil($value));
+
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $value); ++$cells;
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $overall); ++$cells;
                 $writer = PHPExcel_IOFactory::createWriter($reader, 'Excel2007');
                 $writer->save(FOLDER_EXCEL.FILE_EXCEL.'.xlsx');
+            }
+            if ($key == "competition") {
+                $value = number_format($value, 2, '.', ',');
+                $competition = level(ceil($value));
 
-                ++$cells;
-                ++$cells;
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $value); ++$cells;
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $competition); ++$cells;
+                $writer = PHPExcel_IOFactory::createWriter($reader, 'Excel2007');
+                $writer->save(FOLDER_EXCEL.FILE_EXCEL.'.xlsx');
+            }
+            if ($key == "estimated_monthly_search") {
+                $value = number_format($value, 1, '.', ',');
+
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $value); ++$cells;
+                $set_value = $reader->getActiveSheet()->setCellValue($cells.($number+1), $volume); ++$cells;
+                $writer = PHPExcel_IOFactory::createWriter($reader, 'Excel2007');
+                $writer->save(FOLDER_EXCEL.FILE_EXCEL.'.xlsx');
             }
         }
 
